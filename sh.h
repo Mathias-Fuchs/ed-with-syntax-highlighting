babf3e0e763e034a0df2346fcb6f61a1257feb03
Mathias Fuchs <mathias@mathiasfuchs.com>
2022-04-07 17:08:40 +0200

    Update sh.h

--- sh.h
+++ sh.h
@@ -1,4 +1,16 @@
-
+/* GNU ed - The GNU line editor - sh.h
+   Copyright (C) 2022 Mathias Fuchs
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 // when compiling sh.cpp (which includes this file), this will give a C-style symbol in the object file
 // when compiling a C file which includes this file, that symbol is made available to that C file
